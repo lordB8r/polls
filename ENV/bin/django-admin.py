d039ccd326a3569e1edfbd5c6e27030b804da405
lordB8r <jp@zavteq.com>
2013-08-15 08:49:04 -0500

    initial commit of django tutorial

---
+++ ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jpbader/Documents/project_Python/django/django_project/ENV/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
